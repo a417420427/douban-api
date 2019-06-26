f3d25d542e3ff0a26ea64be6212972eb89443694
张帆 <zhangfan@zhangfandeMacBook-Pro.local>
2019-06-26 20:20:41 +0800

    添加音乐分类接口

--- api/music/index.js
+++ api/music/index.js
@@ -2,6 +2,22 @@ import Router from 'koa-router'
 import axios, { getParams } from '../../utils/request'
 import { getMusicDetail } from '../../utils/content';
 const router = new Router()
+
+
+// 分类
+router.get('/types', async ctx => {
+    ctx.body = [
+        { "link": "/music/pop", "name": "流行" },
+        { "link": "/music/rock", "name": "摇滚" },
+        { "link": "/music/folk", "name": "民谣" },
+        { "link": "/music/indie", "name": "独立" },
+        { "link": "/music/chinese", "name": "华语" },
+        { "link": "/music/western", "name": "欧美" },
+        { "link": "/music/japan", "name": "日本" },
+        { "link": "/music/korean", "name": "韩国" }
+    ]
+})
+
 // 34432027
 router.get('/cn', async ctx => {
     const query = ctx.request.query
